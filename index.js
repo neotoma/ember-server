93a93a3e7f463c0ebe81c141f39254ce5051643d
Mark Hendrickson <markmhendrickson@gmail.com>
2017-10-24 19:17:06 +0200

    Integrate fastboot-express-middleware

--- index.js
+++ index.js
@@ -6,20 +6,17 @@ var ranger = require('park-ranger')();
 
 var debug = require('debug')('ember-server');
 var express = require('express');
+var fastbootMiddleware = require('fastboot-express-middleware');
 var http = require('http');
 var https = require('https');
 var path = require('path');
-var prerenderNode = require('prerender-node');
 
 var app = express();
 
-app.use(prerenderNode);
 app.use('/assets', express.static(path.resolve(process.env.EMBER_SERVER_APP_DIR, 'assets')));
 app.use('/bower_components', express.static(path.resolve(process.env.EMBER_SERVER_APP_DIR, 'bower_components')));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.resolve(process.env.EMBER_SERVER_APP_DIR, 'index.html'));
-});
+app.get('*', fastbootMiddleware(process.env.EMBER_SERVER_APP_DIR));
 
 https.createServer(ranger.cert, app).listen(process.env.EMBER_SERVER_HTTPS_PORT, () => {
   debug('Ember server started listening for HTTPS requests', { port: process.env.EMBER_SERVER_HTTPS_PORT });
